3007bdfd7ddbfd7923143a897a520999ccd62f71
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-01-15 18:31:47 +0800

    chnage out put to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', 
+  adapter: cloudflare(),
   integrations: [
     react(),
     tailwind({
