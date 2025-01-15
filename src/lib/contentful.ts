17816b4a9e95dd44745a9f2a7eb4f3ebe94fa32e
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-01-15 18:49:49 +0800

    Deployment fix test:
    Remove FAQ section

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,6 +1,8 @@
 import * as contentfulClient from 'contentful'
 import contentfulManagement from 'contentful-management'
+import type { IMisc } from './interface';
 
+export const prerender = false;
 
 export const client = contentfulClient.createClient({
   space: import.meta.env.PUBLIC_CONTENTFUL_SPACE_ID || '',
@@ -12,3 +14,7 @@ export const managementClient = contentfulManagement.createClient(
     accessToken: import.meta.env.PUBLIC_CONTENTFUL_CMA_TOKEN || '',
   }
 )
+
+export const miscData = await client.withoutUnresolvableLinks.getEntry<IMisc>(
+  "4EGtKjQZn22kzgtVW3hlpl"
+);
