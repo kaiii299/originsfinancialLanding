ca51b7fa6a3fc5b407eb7a2b29b9b3ad2db41882
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-01-15 18:32:23 +0800

    CLOUDFLARE RAHHH

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', 
+  output: 'server',
   adapter: cloudflare(),
   integrations: [
     react(),
